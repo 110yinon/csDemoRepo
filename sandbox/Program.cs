d17adf15fcfe9787222098e6f86e1f70298c655a
winToGoUser <noMail>
2022-08-02 14:54:30 +0300

    delegate example1

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -6,51 +6,19 @@ using System.Threading.Tasks;
 
 namespace sandbox
 {
+    public delegate void myDlgt(string str);
     internal class Program
     {
         static void Main(string[] args)
         {
-            //var arr = new int[] { 1, 2, 3, 4 };
-            var arr = new Person[]
-            {
-                new Person(){FirstName = "avi",LastName="1"},
-                new Person(){FirstName = "benny",LastName="2"},
-                new Person(){FirstName = "charlie",LastName="3"}
-            };
-            var enumerator = arr.GetEnumerator<Person>();
-            while (enumerator.MoveNext())
-            {
-                // opt 1 - casting
-                Console.WriteLine(((Person)enumerator.Current).FirstName);
-                //Or print by the toString()
-                Console.WriteLine(enumerator.Current);
-            }
-
-            //while (enumerator.MoveNext())
-            //{
-            //    // opt 1 - casting
-            //    Console.WriteLine(((Person)enumerator.Current).FirstName);
-            //    //Or print by the toString()
-            //    Console.WriteLine(enumerator.Current);
-            //}
-
-
-            //foreach (var item in arr)
-            //{
-            //    Console.WriteLine(item);
-
-            //}
+            SayHelloTo("bb");
+            var kuni = new myDlgt(SayHelloTo);
+            kuni("sara");
 
         }
-    }
-}
-public class Person
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    public override string ToString()
-    {
-        return $"Person: {this.FirstName}, {this.LastName}";
+        static void SayHelloTo(string name)
+        {
+            Console.WriteLine($"hello {name}");
+        }
     }
 }
